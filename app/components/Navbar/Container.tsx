6ec9a7e8e610fcc34d96b176d2085fc2c71d3437
Vikas Kumar <117701126+CoderVikas94@users.noreply.github.com>
2023-04-17 16:08:01 +0530

    fae

---
+++ app/components/Navbar/Container.tsx
@@ -0,0 +1,10 @@
+interface Containerprops {
+    children: React.ReactNode;
+}
+const Container:React.FC<Containerprops> = ({children}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Container
