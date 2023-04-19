484d7fe10f716f2990c33db448664f72ce57a389
Vikas Kumar <117701126+CoderVikas94@users.noreply.github.com>
2023-04-19 17:37:02 +0530

    dfaesdfaes

--- public/pages/api/[...nextauth].ts
+++ public/pages/api/[...nextauth].ts
@@ -22,10 +22,18 @@ export const authOptions: AuthOptions = {
             credentials: {
                 email : {label : 'email' ,type : 'text'},
                 password: {label : 'password', type:'password'}
-            },async authorize('credentials') {
+            },
+            async authorize('credentials') {
+                
                 if(!credentials?.email || !credentials?.password) {
                     throw new Error("Invalid credentials")
-                }
+                };
+
+                const user = await prisma.user.findUnique({
+                    where :{
+                        email : credentials.email
+                    }
+                });
             }
            })
 
